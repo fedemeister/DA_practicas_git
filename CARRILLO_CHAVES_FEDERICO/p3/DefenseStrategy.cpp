f1528a09f5b0a9bc033fdb1eb23c25c26a09e071
Federico Carrillo Chaves <fedemeister@outlook.es>
2020-12-05 13:30:09 +0100

    p3/DefenseStrategy.cpp ready

--- CARRILLO_CHAVES_FEDERICO/p3/DefenseStrategy.cpp
+++ CARRILLO_CHAVES_FEDERICO/p3/DefenseStrategy.cpp
@@ -16,7 +16,9 @@ RAND_TYPE SimpleRandomGenerator::a;
 #include <queue>
 
 using namespace Asedio;
+
 #include "cronometro.h"
+
 // Estructura que representa una celda candidata
 // valor - valor de la celda
 // vector3 - coordenadas de la celda en Vector3
@@ -25,6 +27,8 @@ class Celda {
 public:
     double valor_;
     Vector3 vector3_;
+
+    Celda() = default;
 
     Celda(double valor, const Vector3 &vector3) : valor_(valor), vector3_(vector3) {}
 };
@@ -46,7 +50,83 @@ struct ComparaValor2 {
         return celda1.valor_ < celda2.valor_;
     }
 };
+/*
+// Algoritmo basado en el pseudocódigo del
+// libro "Introduction to Algorithms 3era edicion página 31"
+void merge(std::vector<Celda> &V, int p, int q, int r) {
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    std::vector<Celda> L(n1), R(n2);
+    for (int i = 0; i < n1; ++i) {
+        L[i].valor_ = V[p + i].valor_;
+    }
+    for (int j = 0; j < n2; ++j) {
+        R[j].valor_ = V[q + j + 1].valor_;
+    }
+    //L[n1] = INT_MAX;
+    //R[n2] = INT_MAX;
+    int i = 0;
+    int j = 0;
+    int z = p;
+    while (i < n1 && j < n2) {
+        //for (int k = p; k < r; ++k) {
+        if (L[i].valor_ >= R[j].valor_) { // ahora ordena de forma decreciente.
+            V[z].valor_ = L[i].valor_;
+            i++;
+        } else {
+            V[z].valor_ = R[j].valor_;
+            j++;
+        }
+        z++;
+    }
+    while (i < n1) {
+        V[z].valor_ = L[i].valor_;
+        i++;
+        z++;
+    }
+    while (j < n2) {
+        V[z].valor_ = R[j].valor_;
+        j++;
+        z++;
+    }
+}
 
+// Algoritmo basado en el pseudocódigo del
+// libro "Introduction to Algorithms 3era edicion página 31"
+void mergeSort(std::vector<Celda> &V, int p, int r) {
+    if (p < r) {
+        int q = (p + r) / 2;
+        mergeSort(V, p, q);
+        mergeSort(V, q + 1, r);
+        merge(V, p, q, r);
+    }
+}
+
+// Algoritmo basado en el pseudocódigo del
+// libro "Introduction to Algorithms 3era edicion página 171"
+int partition(std::vector<Celda> &V, int p, int r) {
+    double x = V[r].valor_;
+    int i = p - 1;
+    for (int j = p; j <= r - 1; ++j) {
+        if (V[j].valor_ >= x) { // ahora ordena de forma decreciente.
+            i = i + 1;
+            std::swap(V[i], V[j]);
+        }
+    }
+    std::swap(V[i + 1], V[r]);
+    return i + 1;
+}
+
+// Algoritmo basado en el pseudocódigo del
+// libro "Introduction to Algorithms 3era edicion página 171"
+void quickSort(std::vector<Celda> &V, int p, int r) {
+    if (p < r) {
+        int q = partition(V, p, r);
+        quickSort(V, p, q - 1);
+        quickSort(V, q + 1, r);
+    }
+}
+*/
 
 // Devuelve la posición en el mapa del centro de la celda (i,j)
 // i - fila
@@ -92,17 +172,19 @@ struct choca {
     }
 };
 
-float defaultCellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight
-    , float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
-    	
+float
+defaultCellValue(int row, int col, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight,
+                 std::list<Object *> obstacles) {
+
     float cellWidth = mapWidth / nCellsWidth;
     float cellHeight = mapHeight / nCellsHeight;
 
-    Vector3 cellPosition((col * cellWidth) + cellWidth * 0.5f, (row * cellHeight) + cellHeight * 0.5f, 0);
-    	
+    //Vector3 cellPosition((col * cellWidth) + cellWidth * 0.5f, (row * cellHeight) + cellHeight * 0.5f, 0);
+    Vector3 cellPosition = cellCenterToPosition(row, col, cellWidth, cellHeight);
+
     float val = 0;
-    for (List<Object*>::iterator it=obstacles.begin(); it != obstacles.end(); ++it) {
-	    val += _distance(cellPosition, (*it)->position);
+    for (auto &obstacle : obstacles) {
+        val += _distance(cellPosition, obstacle->position);
     }
 
     return val;
@@ -134,38 +216,20 @@ bool factibilidad(const std::vector<Defense *> &defensasColocadas, float mapWidt
     return true;
 }
 
-void DEF_LIB_EXPORTED placeDefenses3(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight
-              , List<Object*> obstacles, List<Defense*> defenses) {
-
-    float cellWidth = mapWidth / nCellsWidth;
-    float cellHeight = mapHeight / nCellsHeight; 
-    int maxAttemps = 1000;
-	cronometro c;
-    long int r = 0;
-    c.activar();
-    do {	
-		List<Defense*>::iterator currentDefense = defenses.begin();
-		while(currentDefense != defenses.end() && maxAttemps > 0) {
-
-			(*currentDefense)->position.x = ((int)(_RAND2(nCellsWidth))) * cellWidth + cellWidth * 0.5f;
-			(*currentDefense)->position.y = ((int)(_RAND2(nCellsHeight))) * cellHeight + cellHeight * 0.5f;
-			(*currentDefense)->position.z = 0; 
-			++currentDefense;
-		}
-		
-		++r;
-    } while(c.tiempo() < 1.0);
-    c.parar();
-    std::cout << (nCellsWidth * nCellsHeight) << '\t' << c.tiempo() / r << '\t' << c.tiempo()*2 / r << '\t' << c.tiempo()*3 / r << '\t' << c.tiempo()*4 / r << std::endl;
+std::vector<Celda>::iterator maximo(std::vector<Celda> &v) {
+    std::vector<Celda>::iterator celda;
+    double aux = -99999.0;
+    for (auto it = v.begin(); it != v.end(); it++) {
+        if (it->valor_ > aux) {
+            aux = it->valor_;
+            celda = it;
+        }
+    }
+    return celda;
 }
 
-
-/*
-
-void DEF_LIB_EXPORTED
-placeDefenses(bool **freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight,
-              std::list<Object *> obstacles, std::list<Defense *> defenses) {
-
+void devoradorMonticulo(int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight,
+                        const std::list<Object *>& obstacles, std::list<Defense *> defenses) {
     float cellWidth;
     cellWidth = mapWidth / (float) nCellsWidth;
     float cellHeight;
@@ -178,7 +242,7 @@ placeDefenses(bool **freeCells, int nCellsWidth, int nCellsHeight, float mapWidt
         for (int j = 0; j < nCellsWidth; j++) {
             aux_vector3 = cellCenterToPosition(i, j, cellWidth, cellHeight);
             Celda aux = Celda(
-                    defaultCellValue(i, j, freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses),
+                    defaultCellValue(i, j, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles),
                     aux_vector3);
             Q.push(aux);
         }
@@ -186,41 +250,68 @@ placeDefenses(bool **freeCells, int nCellsWidth, int nCellsHeight, float mapWidt
 
     std::vector<Defense *> defensesPlaced;
     auto defensaCandidata = defenses.begin();
-    bool CEM_colocado = false;
 
-    // CEM
-    while (defensaCandidata != defenses.end() && !CEM_colocado) {
+    while (defensaCandidata != defenses.end()) {
         Celda celda = Q.top();
         Q.pop();
         (*defensaCandidata)->position = celda.vector3_;
         if (factibilidad(defensesPlaced, mapWidth, mapHeight, (*defensaCandidata), obstacles)) {
             defensesPlaced.push_back((*defensaCandidata));
             ++defensaCandidata;
-            CEM_colocado = true;
         }
     }
 
-    // demás defensas
-    std::priority_queue<Celda, std::vector<Celda>, ComparaValor2> Q2;   // nueva cola de prioridad
+}
+
+void devoradorSinOrdenar(bool **freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight,
+                         const std::list<Object *> &obstacles, const std::list<Defense *> &defenses, float cellWidth,
+                         float cellHeight) {
+    std::vector<Celda> celdas_candidatas;
+    Vector3 aux_vector3;
     for (int i = 0; i < nCellsHeight; i++) {
         for (int j = 0; j < nCellsWidth; j++) {
             aux_vector3 = cellCenterToPosition(i, j, cellWidth, cellHeight);
-            Celda aux_celda = Celda(
-                    defaultCellValue(i, j, freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses),
+            Celda aux = Celda(
+                    defaultCellValue(i, j, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles),
                     aux_vector3);
-            Q2.push(aux_celda);
+            celdas_candidatas.push_back(aux);
         }
     }
-    //defenses.sort([](const Defense *a, const Defense *b) { return a->health < b->heath; });
 
-    while (defensaCandidata != defenses.end()) {
-        Celda celda = Q2.top();
-        Q2.pop();
-        (*defensaCandidata)->position = celda.vector3_;
+    //auto max = std::max_element(celdas_candidatas.begin(), celdas_candidatas.end(),
+    //                            [](const Celda &a, const Celda &b) {
+    //                                return a.valor_ < b.valor_;
+    //                            });
+
+    std::vector<Defense *> defensesPlaced;
+    auto defensaCandidata = defenses.begin();
+
+    while (defensaCandidata != defenses.end() && !celdas_candidatas.empty()) {
+        auto celda = maximo(celdas_candidatas);
+        celdas_candidatas.erase(celda);
+        (*defensaCandidata)->position = celda->vector3_;
         if (factibilidad(defensesPlaced, mapWidth, mapHeight, (*defensaCandidata), obstacles)) {
             defensesPlaced.push_back((*defensaCandidata));
-            ++defensaCandidata;
+            defensaCandidata++;
         }
     }
-*/
+}
 
+void DEF_LIB_EXPORTED
+placeDefenses3(bool **freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight,
+               const List<Object *> obstacles, const List<Defense *> defenses) {
+
+    float cellWidth;
+    cellWidth = mapWidth / (float) nCellsWidth;
+    float cellHeight;
+    cellHeight = mapHeight / (float) nCellsHeight;
+
+    if (defenses.size() <= 15 && mapWidth <= 500) {
+        devoradorSinOrdenar(freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses, cellWidth,
+                            cellHeight);
+    } else {
+        devoradorMonticulo(nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
+    }
+
+
+}
